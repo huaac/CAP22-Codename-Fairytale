db73857d0dab99316e497e98ab78b5b4ebb65010
Aissa Akiyama <arcticamaroq@gmail.com>
2022-12-07 18:17:12 -0800

    disable falling rock collider on impact
    - player doesn't get hurt by rock already fallen
    - rock bounces

--- Codename Fairytale/Assets/Scripts/Enemy/FallingRock.cs
+++ Codename Fairytale/Assets/Scripts/Enemy/FallingRock.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class FallingRock : MonoBehaviour
 {
     [SerializeField] private int attack = 10;
+    private bool hasLanded = false;
+    private Collider2D m_collider;
+
+    private void Awake()
+    {
+        m_collider = GetComponent<Collider2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent(out PlayerHealth player))
         {
-            if (!player.WasJustDamaged)
+            if (!player.WasJustDamaged && !hasLanded)
             {
                 player.TakeDamage(attack);
             }
         }
+        hasLanded = true;
+        m_collider.enabled = false;
         Destroy(gameObject, 2.0f);
     }
-
-    
 }
