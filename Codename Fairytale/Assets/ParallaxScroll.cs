1d11cd8e7404ccc034623a1756815b5ab01ee24a
Aissa Akiyama <arcticamaroq@gmail.com>
2023-02-05 09:56:23 -0800

    infinite parallax scrolling

--- Codename Fairytale/Assets/ParallaxScroll.cs
+++ Codename Fairytale/Assets/ParallaxScroll.cs
@@ -2,59 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Infinite parallax scrolling for a background sprite.
+/// Sprite must be a child object of the camera, and for infinite scrolling must be 3 units long.
+/// </summary>
+
 public class ParallaxScroll : MonoBehaviour
 {
-    [SerializeField] private float parallax;
+    [Tooltip("0 = no scroll, sprite stays still\n1 = scolls as fast as foreground")]
+    [SerializeField][Range(0, 1)] private float parallax;
+
     private Transform cam;
-    private SpriteRenderer sprite;
+    private Vector3 prevCam;
 
-    private float startL, startR;
     private float length;
-
-    private Vector3 oldCam;
-    private float camWidthHalf;
-
-    private bool madeNewRight, madeNewLeft;
 
     private void Awake()
     {
         cam = Camera.main.transform;
-        camWidthHalf = Camera.main.orthographicSize * Camera.main.aspect / 2;
-        oldCam = cam.position;
+        prevCam = cam.position;
 
-        sprite = GetComponent<SpriteRenderer>();
-        length = sprite.bounds.size.x;
-        startL = transform.position.x - (length / 2);
-        startR = transform.position.x + (length / 2);
+        length = GetComponent<SpriteRenderer>().bounds.size.x / 3;
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        float distance = (oldCam.x - cam.position.x) * parallax;
-        Vector3 newPos = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
-        transform.position = newPos;
+        // parallax
+        Vector3 distance = (prevCam - cam.position) * parallax;
+        transform.position += distance;
 
-        oldCam = cam.position;
+        prevCam = cam.position;
 
-        if (!madeNewRight && startL < cam.position.x - camWidthHalf)
+        // infinite scrolling
+        if (cam.position.x - transform.position.x >= length)
         {
-            Instantiate(gameObject, new Vector3(transform.position.x + length, transform.position.y, transform.position.z), transform.rotation);
-            madeNewRight = true;
+            transform.position = new Vector3(cam.position.x, transform.position.y, transform.position.z);
         }
-        if (startR < cam.position.x - camWidthHalf)
-        {
-            Destroy(this.gameObject);
-        }
-
-        /*
-        if (!madeNewLeft && startL > cam.position.x - camWidthHalf)
-        {
-            Instantiate(gameObject, new Vector3(transform.position.x - length, transform.position.y, transform.position.z), transform.rotation);
-            madeNewLeft = true;
-        }
-        if (startR > cam.position.x + camWidthHalf)
-        {
-            Destroy(this.gameObject);
-        }*/
     }
 }
