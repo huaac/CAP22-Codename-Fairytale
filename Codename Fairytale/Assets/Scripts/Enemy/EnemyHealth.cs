2b286b0a6bb88c8503db4e3b633a9b425701b736
Esme-gif <esmejuarez100@gmail.com>
2023-03-08 12:43:28 -0800

    commented out the sprite changes I did before to indicate enemy not being able to get stunned
    reverted it back to figure out what can be done now that animations are put in

--- Codename Fairytale/Assets/Scripts/Enemy/EnemyHealth.cs
+++ Codename Fairytale/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -9,7 +9,7 @@ public class EnemyHealth : MonoBehaviour, IDamageable
     [SerializeField] private float stunTime = 5f;
     [SerializeField] private Slider healthBar;
     public bool cantStunYet;
-    private SpriteRenderer quad; // quad to change its opacity
+    //private SpriteRenderer quad; // quad to change its opacity
 
     [SerializeField] private int maxHealth = 100;
     private int currentHealth;
@@ -22,7 +22,7 @@ public class EnemyHealth : MonoBehaviour, IDamageable
     private void Awake()
     {
         enemyMovement = GetComponent<IEnemy>();
-        quad = this.GetComponent<SpriteRenderer>();
+        //quad = this.GetComponent<SpriteRenderer>();
 
         currentHealth = maxHealth;
 
@@ -69,13 +69,13 @@ public class EnemyHealth : MonoBehaviour, IDamageable
         
         cantStunYet = true;
 
-        Color oldColor =  quad.color; // old color 
-        quad.color = new Color(oldColor.r, oldColor.g, oldColor.b, 0.5f);
+        //Color oldColor =  quad.color; // old color 
+        //quad.color = new Color(oldColor.r, oldColor.g, oldColor.b, 0.5f);
 
         yield return new WaitForSeconds(stunTime);
         cantStunYet = false;
         
-        quad.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1f);
+        //quad.color = new Color(oldColor.r, oldColor.g, oldColor.b, 1f);
 
     }
 
